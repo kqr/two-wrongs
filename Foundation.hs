cc9b296903ca462c49c22c956f54cbc92d102db2
kqr <k@rdw.se>
2014-10-26 21:27:48 +0100

    Initial commit

---
+++ Foundation.hs
@@ -0,0 +1,164 @@
+module Foundation where
+
+import Prelude
+import Yesod
+import Yesod.Static
+import Yesod.Auth
+import Yesod.Auth.OpenId
+import Yesod.Default.Config
+import Yesod.Default.Util (addStaticContentExternal)
+import Network.HTTP.Client.Conduit (Manager, HasHttpManager (getHttpManager))
+import qualified Settings
+import Settings.Development (development)
+import qualified Database.Persist
+import Database.Persist.Sql (SqlBackend)
+import Settings.StaticFiles
+import Settings (widgetFile, Extra (..))
+import Model
+import Text.Jasmine (minifym)
+import Text.Hamlet (hamletFile)
+import Yesod.Core.Types (Logger)
+import Data.Text (Text)
+
+
+-- | The site argument for your application. This can be a good place to
+-- keep settings and values requiring initialization before your application
+-- starts running, such as database connections. Every handler will have
+-- access to the data present here.
+data App = App
+    { settings :: AppConfig DefaultEnv Extra
+    , getStatic :: Static -- ^ Settings for static file serving.
+    , connPool :: Database.Persist.PersistConfigPool Settings.PersistConf -- ^ Database connection pool.
+    , httpManager :: Manager
+    , persistConfig :: Settings.PersistConf
+    , appLogger :: Logger
+    }
+
+instance HasHttpManager App where
+    getHttpManager = httpManager
+
+-- This is where we define all of the routes in our application. For a full
+-- explanation of the syntax, please see:
+-- http://www.yesodweb.com/book/routing-and-handlers
+--
+-- Note that this is really half the story; in Application.hs, mkYesodDispatch
+-- generates the rest of the code. Please see the linked documentation for an
+-- explanation for this split.
+mkYesodData "App" $(parseRoutesFile "config/routes")
+
+type Form x = Html -> MForm (HandlerT App IO) (FormResult x, Widget)
+
+-- Please see the documentation for the Yesod typeclass. There are a number
+-- of settings which can be configured by overriding methods here.
+instance Yesod App where
+    approot = ApprootMaster $ appRoot . settings
+
+    -- Store session data on the client in encrypted cookies,
+    -- default session idle timeout is 120 minutes
+    makeSessionBackend _ = fmap Just $ defaultClientSessionBackend
+        120    -- timeout in minutes
+        "config/client_session_key.aes"
+
+    defaultLayout widget = do
+        master <- getYesod
+        mmsg <- getMessage
+
+        -- We break up the default layout into two components:
+        -- default-layout is the contents of the body tag, and
+        -- default-layout-wrapper is the entire page. Since the final
+        -- value passed to hamletToRepHtml cannot be a widget, this allows
+        -- you to use normal widget features in default-layout.
+
+        pc <- widgetToPageContent $ do
+--            addStylesheet $ StaticR css_bootstrap_css
+            addStylesheetRemote "http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+            addStylesheetRemote "http://cloud.webtype.com/css/bfc07ec5-8efd-4b0d-b1e1-7fb291d9edb0.css"
+            $(widgetFile "standard")
+        withUrlRenderer $(hamletFile "templates/base.hamlet")
+
+    -- This is done to provide an optimization for serving static files from
+    -- a separate domain. Please see the staticRoot setting in Settings.hs
+    urlRenderOverride y (StaticR s) =
+        Just $ uncurry (joinPath y (Settings.staticRoot $ settings y)) $ renderRoute s
+    urlRenderOverride _ _ = Nothing
+
+    -- The page to be redirected to when authentication is required.
+    authRoute _ = Just $ AuthR LoginR
+
+
+    -- Routes requiring authentication include *any* write request
+    isAuthorized _         True = requiresAuthor
+    isAuthorized AuthorsR  _    = requiresAuthor
+    isAuthorized NewR      _    = requiresAuthor
+
+    -- All other routes are open to the world
+    isAuthorized _         _    = return Authorized
+
+
+    -- This function creates static content files in the static folder
+    -- and names them based on a hash of their content. This allows
+    -- expiration dates to be set far in the future without worry of
+    -- users receiving stale content.
+    addStaticContent =
+        addStaticContentExternal minifym genFileName Settings.staticDir (StaticR . flip StaticRoute [])
+      where
+        -- Generate a unique filename based on the content itself
+        genFileName lbs
+            | development = "autogen-" ++ base64md5 lbs
+            | otherwise   = base64md5 lbs
+
+    -- Place Javascript at bottom of the body tag so the rest of the page loads first
+    jsLoader _ = BottomOfBody
+
+    -- What messages should be logged. The following includes all messages when
+    -- in development, and warnings and errors in production.
+    shouldLog _ _source level =
+        development || level == LevelWarn || level == LevelError
+
+    makeLogger = return . appLogger
+
+
+-- How to run database actions.
+instance YesodPersist App where
+    type YesodPersistBackend App = SqlBackend
+    runDB = defaultRunDB persistConfig connPool
+instance YesodPersistRunner App where
+    getDBRunner = defaultGetDBRunner connPool
+
+
+instance YesodAuth App where
+    type AuthId App = Text
+    authPlugins _ = [authOpenId OPLocal []]
+
+    getAuthId = return . Just . credsIdent
+    maybeAuthId = lookupSession credsKey
+
+    loginHandler = lift (defaultLayout $(widgetFile "login"))
+    loginDest _ = HomeR
+    logoutDest _ = HomeR
+
+    authHttpManager = httpManager
+
+isAuthor :: Handler Bool
+isAuthor =
+  maybeAuthId >>= maybe (return False) (\uid ->
+    fmap (>0) (runDB (count [AuthorsUid ==. uid])))
+
+requiresAuthor :: Handler AuthResult
+requiresAuthor = do
+  author <- isAuthor
+  return $ if author
+     then Authorized
+     else Unauthorized "You must be an identified author"
+  
+
+
+-- This instance is required to use forms. You can modify renderMessage to
+-- achieve customized and internationalized form validation messages.
+instance RenderMessage App FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+-- | Get the 'Extra' value, used to hold data from the settings.yml file.
+getExtra :: Handler Extra
+getExtra = fmap (appExtra . settings) getYesod
+
---
+++ Handler/Site.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Handler.Site where
+
+import Import
+import Yesod.Form.Bootstrap3
+import Data.Time.Calendar (showGregorian)
+
+
+
+getHomeR :: Handler Html
+getHomeR = do
+  entries <- fmap (map entityVal) (runDB (selectList [EntryPublished !=. Nothing] [Desc EntryPublished]))
+  defaultLayout $ do
+    setTitle "Two Wrongs"
+    $(widgetFile "homepage")
+
+
+
+getEntryR :: Text -> Handler Html
+getEntryR slug = do
+  entry <- fmap entityVal (runDB (getBy404 (UniqueSlug slug)))
+  author <- isAuthor
+  defaultLayout $ do
+    setTitle ("Two Wrongs :: " <> toHtml (entryTitle entry))
+    $(widgetFile "detail")
+
+
+
+entryForm :: Maybe Entry -> Form Entry
+entryForm existing = renderBootstrap3 BootstrapBasicForm $
+  Entry <$> areq textField (bfs ("Title: " :: Text)) (fmap entryTitle existing)
+        <*> areq textField (bfs ("Slug: " :: Text)) (fmap entrySlug existing)
+        <*> aopt dayField (bfs ("Published: " :: Text)) (fmap entryPublished existing)
+        <*> areq htmlField (bfs ("Content: " :: Text)) (fmap entryContent existing)
+
+getNewR :: Handler Html
+getNewR = do
+  let next = NewR
+  (formFields, enctype) <- generateFormPost (entryForm Nothing)
+  defaultLayout $ do
+    setTitle "Two Wrongs :: Create New Entry"
+    $(widgetFile "entryform")
+
+postNewR :: Handler Html
+postNewR = do
+  let next = NewR
+  ((result, formFields), enctype) <- runFormPost (entryForm Nothing)
+  case result of
+    FormSuccess entry -> do
+      exists <- runDB (insertUnique entry)
+      case exists of
+        Just _  -> setMessage "Blog entry successfully added" >> redirect HomeR
+        Nothing -> setMessage "That slug already leads to a different entry"
+    _ -> setMessage "Invalid form submission, check for errors"
+        
+  defaultLayout $ do
+    setTitle "Two Wrongs :: Create New Entry"
+    $(widgetFile "entryform")
+
+
+getEditR :: Text -> Handler Html
+getEditR slug = do
+  let next = EditR slug
+  entry <- fmap entityVal (runDB (getBy404 (UniqueSlug slug)))
+  (formFields, enctype) <- generateFormPost (entryForm (Just entry))
+  defaultLayout $ do
+    setTitle "Two Wrongs :: Edit Entry"
+    $(widgetFile "entryform")
+
+postEditR :: Text -> Handler Html
+postEditR slug = do
+  let next = EditR slug
+  ((result, formFields), enctype) <- runFormPost (entryForm Nothing)
+  case result of
+    FormSuccess entry -> do
+      key <- fmap entityKey (runDB (getBy404 (UniqueSlug slug)))
+      runDB (repsert key entry)
+      setMessage "Blog entry successfully edited"
+      redirect HomeR
+    _ -> setMessage "Invalid form submission, check for errors"
+        
+  defaultLayout $ do
+    setTitle "Two Wrongs :: Create New Entry"
+    $(widgetFile "entryform")
+
+
+
+authorForm :: Form Authors
+authorForm = renderBootstrap3 BootstrapBasicForm $
+  Authors <$> areq textField (bfs ("UID: " :: Text)) Nothing
+
+getAuthorsR :: Handler Html
+getAuthorsR = do
+  (formFields, enctype) <- generateFormPost authorForm
+
+  authors <- fmap (map entityVal) (runDB (selectList [] [Desc AuthorsId]))
+  defaultLayout $ do
+    setTitle "Two Wrongs :: Authorized Authors"
+    $(widgetFile "authors")
+
+postAuthorsR :: Handler Html
+postAuthorsR = do
+  ((result, formFields), enctype) <- runFormPost authorForm
+  formFailure <- case result of
+    FormSuccess author -> do
+      exists <- runDB (insertUnique author)
+      case exists of
+        Just _  -> setMessage "Author successfully added!" >> redirect AuthorsR
+        Nothing -> setMessage "An author with that UID already exists"
+    _ -> setMessage "Invalid form submission, check for errors"
+        
+  authors <- fmap (map entityVal) (runDB (selectList [] [Desc AuthorsId]))
+  defaultLayout $ do
+    setTitle "Two Wrongs :: Authorized Authors"
+    $(widgetFile "authors")
+
+
+
+
